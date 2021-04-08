f566f07b5eb5a217ef82de126f76dd86cf51b832
onealmond <one.awesome.peanut@gmail.com>
2021-04-08 13:23:54 +0800

    add header

---
+++ js/common.js
@@ -0,0 +1,6 @@
+window.onscroll = function() {
+if(window.scrollY >= 450) {
+  document.getElementById('back-to-top').style.visibility="visible";
+} else {
+  document.getElementById('back-to-top').style.visibility="hidden";
+}};
