0879e9fcc95a12a737861bb7eb23d4f73beba08a
onealmond <one.awesome.peanut@gmail.com>
2021-04-12 13:13:20 +0800

    [wip] md2html

---
+++ scripts/md2html.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ -z $1 ]] ;then echo "input file not given"; exit 0; fi
+if [[ -z $2 ]] ;then echo "output file not given"; exit 0; fi
+if [[ -z $3 ]] ;then 
+  echo "title not given, using space instead"
+  title=" "
+else
+  title="$3"
+fi
+
+pandoc --from=markdown --to=html \
+  -o "$2" \
+  --metadata title="$title" \
+  -H templates/writeup-head.html \
+  -B templates/writeup-before-body.html \
+  -A templates/writeup-after-body.html \
+  "$1"
